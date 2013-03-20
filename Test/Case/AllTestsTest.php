da61c00c32e0a8f4fe95ff1a730269357d0611a0
milds <tatata2655@gmail.com>
2013-03-20 19:58:35 +0900

    alltest

--- Test/Case/AllTestsTest.php
+++ Test/Case/AllTestsTest.php
@@ -13,10 +13,10 @@ class AllTests extends PHPUnit_Framework_TestSuite {
 	public static function suite() {
 
 		$suite = new PHPUnit_Framework_TestSuite('All Tests');
-		$suite->addTestFile(__DIR__.'Controller'.DS. 'ImageControllerTest.php');
-		$suite->addTestFile(__DIR__.'Model'.'ImageTest.php');
-		$suite->addTestFile(__DIR__.'Model'.DS.'Behavior'.DS.'UploadBehaviorTest.php');
-		$suite->addTestFile(__DIR__.'View'.DS.'Helper'.DS.'ImageTableHelperTest.php');
+		$suite->addTestFile(__DIR__.DS.'Controller'.DS. 'ImageControllerTest.php');
+		$suite->addTestFile(__DIR__.DS.'Model'.'ImageTest.php');
+		$suite->addTestFile(__DIR__.DS.'Model'.DS.'Behavior'.DS.'UploadBehaviorTest.php');
+		$suite->addTestFile(__DIR__.DS.'View'.DS.'Helper'.DS.'ImageTableHelperTest.php');
 		return $suite;
 	}
 }
